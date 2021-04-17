ea7d4ee44b28666deb839b2c456d2b8346b397a3
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-04-17 22:42:04 +0300

    Test

--- src/tests/ForecastSummary.test.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-describe("ForecastSummary", () => {
-  it("renders", () => {});
-});
---
+++ src/tests/components/ForecastSummary.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot();
+  });
+
+  it("render correct values for props", () => {
+    const { getByText } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+    expect(getByText("Stub description")).toHaveClass("forecast-summary__description");
+    expect(getByText("stubIcon")).toHaveClass("forecast-summary__icon");
+    expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
+  });
+});
