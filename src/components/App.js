67ddb0f5097edc793caa665665002f8890cad121
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-04-25 22:52:58 +0300

    progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import "../styles/App.css";
 
-const App = (props) => {
-  const { forecasts, location } = props;
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+const App = () => {
+  const [forecasts, setForecasts] = useState([]);
+  const [location, setLocation] = useState({ city: "", country: "" });
+  const [selectedDate, setSelectedDate] = useState(0);
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate,
   );
@@ -21,27 +21,9 @@ const App = (props) => {
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
       />
-      <ForecastDetails forecast={selectedForecast} />
+      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
     </div>
   );
-};
-
-App.propTypes = {
-  forecasts: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.number,
-      description: PropTypes.string,
-      icon: PropTypes.number,
-      temperature: PropTypes.shape({
-        max: PropTypes.number,
-        min: PropTypes.number,
-      }),
-    }),
-  ).isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
-import { location, forecasts } from './data/forecast.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App forecasts={forecasts} location={location} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 );
